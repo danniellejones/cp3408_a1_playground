2f4197aec82064c7d32c63ae2d3402f5af9bbcc9
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-05-26 07:34:24 +1000

    Update Game Manager to use audio in start menu

--- GamePlayground/Assets/UIGame/GameManager.cs
+++ GamePlayground/Assets/UIGame/GameManager.cs
@@ -3,10 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
     public static GameManager instance;
 
-    // World Values to track - public accessible methods below
+    // World Values to track
     private int levelNumber = 1;
     private int numberOfKeys = 0;
 
@@ -17,6 +16,7 @@ public class GameManager : MonoBehaviour
     private GameObject deathMenuUI;
     private GameObject gamePlayUI;
 
+    [Header("Scene")]
     public string sceneToLoad;
 
     // Set up audio for UI Menus
@@ -38,20 +38,43 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        gameManagerAudioSource = gamePlayUI.GetComponent<AudioSource>();
+        gameManagerAudioSource = this.GetComponent<AudioSource>();
+
 
         //startMenuUI = startMenuUI.GetComponent<GameObject>();
         //startMenuUI.SetActive(true);
-        //Scene currentScene = SceneManager.GetActiveScene();
-        //string sceneName = currentScene.name;
-        //if (currentScene.name == "startMenu")
-        //{
-        //    PlayAudioClip(GetRandomAudioClip(startMenuAudioClips), true);
-        //}
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if (currentScene.name == "StartMenu")
+        {
+            PlayAudioClip(GetRandomAudioClip(startMenuAudioClips), true);
+        }
     }
 
+    private void Start()
+    {
+        //gameManagerAudioSource = this.GetComponent<AudioSource>();
+    }
+
+    // On start menu if Play is selected continue saved from save
+    public void ContinueGame()
+    {
+        levelNumber = PlayerPrefs.HasKey("LevelNumber") ? PlayerPrefs.GetInt("LevelNumber") : 1;
+        numberOfKeys = PlayerPrefs.HasKey("NumberOfKeys") ? PlayerPrefs.GetInt("NumberOfKeys") : 0;
+        StartGame();
+    }
+
+    // On start menu if New Game is selected
+        public void StartNewGame()
+    {
+        ResetLevelNumber();
+        StartGame();
+    }
+
+    // Start game
     public void StartGame()
     {
+        PlayAudioClip(selectButtonAudioClip, false);
         SceneManager.LoadScene(sceneToLoad);
 
         // Assign UI instances to Game Manager
@@ -69,11 +92,13 @@ public class GameManager : MonoBehaviour
         deathMenuUI.SetActive(false);
     }
 
+    // Increment level
     public void incrementLevel()
     {
         levelNumber++;
     }
 
+    // Display level complete UI
     public void onSuccessfulLevelCompletion()
     {
         gamePlayUI.SetActive(false);
@@ -81,6 +106,7 @@ public class GameManager : MonoBehaviour
         incrementLevel();
     }
 
+    // Select next level button from End Level UI
     public void LoadNextLevel()
     {
         PlayAudioClip(selectButtonAudioClip, false);
@@ -89,6 +115,7 @@ public class GameManager : MonoBehaviour
         gamePlayUI.SetActive(true);
     }
 
+    // Select try again button from Death Menu UI
     public void RestartLevel()
     {
         PlayAudioClip(selectButtonAudioClip, false);
@@ -97,6 +124,7 @@ public class GameManager : MonoBehaviour
         gamePlayUI.SetActive(true);
     }
 
+    // Display Death Menu UI on player death
     public void GameOver()
     {
         PlayAudioClip(onDeathAudioClip, false);
@@ -105,6 +133,7 @@ public class GameManager : MonoBehaviour
         deathMenuUI.SetActive(true);
     }
 
+    // Save level and keys, then exit game
     public void ExitGame()
     {
         PlayAudioClip(selectButtonAudioClip, false);
@@ -113,6 +142,7 @@ public class GameManager : MonoBehaviour
         Application.Quit();
     }
 
+    // Open pause menu and pause game
     public void PauseGame()
     {
         PlayAudioClip(selectButtonAudioClip, false);
@@ -122,6 +152,7 @@ public class GameManager : MonoBehaviour
         worldMusicPlayer.SetWorldState(WorldMusicPlayer.WorldState.InMenu);
     }
 
+    // Resume game from pause menu
     public void ResumeGame()
     {
         PlayAudioClip(selectButtonAudioClip, false);
@@ -131,21 +162,25 @@ public class GameManager : MonoBehaviour
         worldMusicPlayer.SetWorldState(WorldMusicPlayer.WorldState.Idle);
     }
 
+    // Reset Level Counter
     public void ResetLevelNumber()
     {
         levelNumber = 1;
     }
 
+    // Get the level number
     public int GetLevelNumber()
     {
         return levelNumber;
     }
 
+    // Get a random audio clip from an array
     private AudioClip GetRandomAudioClip(AudioClip[] audioClips)
     {
         return audioClips[Random.Range(0, audioClips.Length)];
     }
 
+    // Play audio clip, nominate looping or not
     private void PlayAudioClip(AudioClip clip, bool loopCondition)
     {
         gameManagerAudioSource.loop = loopCondition;
